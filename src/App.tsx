b670fa26763f691339075e8ef86df7d4023f9f77
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2025-03-01 00:28:35 +0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import MainLayout from "@rt/layouts/MainLayout/MainLayout";
 import { Amplify } from "aws-amplify";
-import awsmobile from "@rt/aws-exports";
 import ContextIndex from "@rt/Context/Index";
+import config from "./aws-exports";
 
-Amplify.configure(awsmobile);
+Amplify.configure(config);
 
 function App() {
   return (
