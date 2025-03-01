c6ca2dadfbf15cb811201f06558e18ef33d8cc82
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2025-03-01 11:04:38 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from "path";
- 
+import 'vite/client';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    
-  ],
-  resolve:{
-    alias:{
-      '@rt':path.resolve(__dirname,'src')
-    }
-  }
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@rt": resolve(__dirname, "src"),
+    },
+  },
+});
