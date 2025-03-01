3795591512f0f4e160a039ea9eeed15489729248
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2025-03-01 10:52:14 +0300

    ts config updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 import MainLayout from "@rt/layouts/MainLayout/MainLayout";
 import { Amplify } from "aws-amplify";
 import ContextIndex from "@rt/Context/Index";
-import config from "./aws-exports";
+import awsmobile from "@rt/aws-exports";
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 function App() {
   return (
